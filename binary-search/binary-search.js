c9d6a1ba2737dfd230611387e586cfafa6bcc987
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-08-17 13:50:52 +0500

    Complete binary search problem

---
+++ binary-search/binary-search.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+const search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    mid = computeMid(left, right);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+const computeMid = (left, right) => {
+  return Math.floor((left + right) / 2);
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
+console.log(search([0, 1, 2, 3, 4], 23)); // -1
